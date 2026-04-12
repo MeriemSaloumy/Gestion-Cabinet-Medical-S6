9fe6d5d05f0cd37913470ef33885d5e55d761b2c
Hajar20062025 <mourjihajar@gmail.com>
2026-04-12 19:10:48 +0100

    Les Flux

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -12,9 +12,9 @@ class AppointmentController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        // Le middleware auth est déjà défini dans les routes
     }
-
+    
     // Vue pour le secrétaire
     public function secretaireIndex()
     {
